0e73e03bef79149972020120377e64eb145d1a72
Fabian Bitta <fabian.bitta@gmail.com>
2018-08-13 17:59:47 +0200

    Set up new rails project including models for pool, user, booking and review

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources: :pools do
+    resources: :bookings, only [ :new, :create, :show ]
+  end
+  resources: :bookings, only [ :destroy ]
 end
