9b376e538a8e8fc9e9000d993524a27f9c121639
Maximilian Gerbaulet <maximilian@gerbaulethh.de>
2018-08-15 16:11:36 +0200

    review test

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,18 @@
 class ReviewsController < ApplicationController
+
+ def show
+    @pool = Pool.find(params[:id])
+    @review = Review.new
+  end
+
   def create
     @pool = Pool.find(params[:pool_id])
     @review = Review.new(review_params)
     @review.pool = @pool
+    @review.user = @user
     if @review.save
       redirect_to pool_path(@pool)
+
     else
       render "pools/show"
     end
