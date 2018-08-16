606978a7fd6224fb7fbc2da6a146be581e498435
Matti Fischer <mr.matti.fischer@googlemail.com>
2018-08-16 17:33:17 +0200

    here comes the map

---
+++ app/javascript/components/map_all.js
@@ -0,0 +1,14 @@
+import mapboxgl from 'mapbox-gl';
+
+if (document.getElementById("map_all")) {
+  mapboxgl.accessToken = 'pk.eyJ1IjoibWF0dGlmaXNjaGVyMSIsImEiOiJjamt3ZHI5bzMweHFjM2twM2JrdGhuZ2NwIn0.aWxD9TcyXXVSTeUCqHe4Ww';
+  var map_all = new mapboxgl.Map({
+  container: 'map_all',
+  center: [13, 52],
+  style: 'mapbox://styles/mapbox/streets-v10',
+  minZoom: 6
+  });
+};
+
+map_all.addControl(new mapboxgl.NavigationControl());
+export { map_all }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_100724) do
+ActiveRecord::Schema.define(version: 2018_08_16_122213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2018_08_15_100724) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "photo"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_pools_on_user_id"
   end
 
